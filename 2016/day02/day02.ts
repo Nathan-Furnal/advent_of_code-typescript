96e845a5caa4a7666786f4ed62b89feac1052c85
Nathan Furnal <nathanfurnal.dev@gmail.com>
2023-11-26 23:45:43 +0100

    types are not okay

--- 2016/day02/day02.ts
+++ 2016/day02/day02.ts
@@ -22,18 +22,23 @@ function inbound(p: Position, d: Direction, n: number): boolean{
     return pos.x >= 0 && pos.y >= 0 && pos.x < n && pos.y < n;
 }
 
-function getSolutions(lines: Direction[][]): string[]{
+function getSolutions(lines: string[]): string[]{
     const code1: string[] = new Array(lines.length).fill('0');
     const code2: string[] = new Array(lines.length).fill('0');
     const pos1: Position = {x: 1, y: 1};
     const pos2: Position = {x: 2, y: 0};
     for (let i = 0; i < lines.length; i++){
-        for(const d of lines[i]){            
+        for(const d of lines[i]){     
+            // @ts-ignore: type-checker not smart enough        
             if (inbound(pos1, d, N1)){
+                // @ts-ignore: type-checker not smart enough        
                 addDirectionToPositionInPlace(pos1, d);
             }
+            // @ts-ignore: type-checker not smart enough        
             const pos2Tmp = addDirectionToPosition(pos2, d);
+            // @ts-ignore: type-checker not smart enough        
             if(inbound(pos2, d, N2) && GRID2[pos2Tmp.x][pos2Tmp.y] != ""){
+                // @ts-ignore: type-checker not smart enough        
                 addDirectionToPositionInPlace(pos2, d);
             }
         }
@@ -44,6 +49,6 @@ function getSolutions(lines: Direction[][]): string[]{
 }
 
 if(import.meta.main){
-    const lines = <unknown> utils.readLinesFromArgs();
-    console.log(getSolutions(<Direction[][]>lines));
+    const lines = utils.readLinesFromArgs();
+    console.log(getSolutions(lines));
 }
