83d231aaa429f8268135c50a244b146e022bbf14
Nathan Furnal <nathanfurnal.dev@gmail.com>
2023-11-26 23:19:24 +0100

    add day2

---
+++ 2016/day02/day02.ts
@@ -0,0 +1,49 @@
+import * as utils from "../../utils.ts";
+import { Position, Direction, addDirectionToPosition, addDirectionToPositionInPlace } from "../common.ts";
+
+const N1 = 3;
+const N2 = 5;
+const GRID1 = [
+    ["1", "2", "3"],
+    ["4", "5", "6"],
+    ["7", "8", "9"]
+]
+const GRID2 = [
+    ["" , "" , "1", "" , "" ],
+    ["" , "2", "3", "4", "" ],
+    ["5", "6", "7", "8", "9"],
+    ["" , "A", "B", "C", "" ],
+    ["" , "" , "D", "" , "" ],
+
+]
+
+function inbound(p: Position, d: Direction, n: number): boolean{
+    const pos = addDirectionToPosition(p, d);
+    return pos.x >= 0 && pos.y >= 0 && pos.x < n && pos.y < n;
+}
+
+function getSolutions(lines: Direction[][]): string[]{
+    const code1: string[] = new Array(lines.length).fill('0');
+    const code2: string[] = new Array(lines.length).fill('0');
+    const pos1: Position = {x: 1, y: 1};
+    const pos2: Position = {x: 2, y: 0};
+    for (let i = 0; i < lines.length; i++){
+        for(const d of lines[i]){            
+            if (inbound(pos1, d, N1)){
+                addDirectionToPositionInPlace(pos1, d);
+            }
+            const pos2Tmp = addDirectionToPosition(pos2, d);
+            if(inbound(pos2, d, N2) && GRID2[pos2Tmp.x][pos2Tmp.y] != ""){
+                addDirectionToPositionInPlace(pos2, d);
+            }
+        }
+        code1[i] = GRID1[pos1.x][pos1.y];
+        code2[i] = GRID2[pos2.x][pos2.y];
+    }
+    return [code1.join(""), code2.join("")];
+}
+
+if(import.meta.main){
+    const lines = <unknown> utils.readLinesFromArgs();
+    console.log(getSolutions(<Direction[][]>lines));
+}
